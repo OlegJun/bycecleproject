a8ad477cd453962234e4f081ebde5de8ad1d724e
OlegJun <olegpotsah290@gmail.com>
2022-08-27 19:20:21 +0500

    fixed and added animation of the burger menu visible-2

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@ import isWebp from "./modules/isWebP.js";
 isWebp();
 /* ---------------------------------------------------------------------------------------------------------------------------- */
 
-   const burger = document.querySelector('.header__menu-burger');
+   const burgerMenu = document.querySelector('.header__menu-burger');
    const headerMenu = document.querySelector('.header__menu');
    const body = document.body;
 
-   burger.addEventListener('click', () => {
+   burgerMenu.addEventListener('click', () => {
       window.scrollTo(0, 0)
       headerMenu.classList.toggle('active')
+      burgerMenu.classList.toggle('active')
       body.classList.toggle('lock')
    });
 
